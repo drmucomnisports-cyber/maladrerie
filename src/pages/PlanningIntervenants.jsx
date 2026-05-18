aada214d6c899fe15a63fd1052fae8f9495ffa4a
David ROUJET <david.roujet@mucomnisports.fr>
2026-05-18 15:54:57 +0200

    fix: imports globaux date-fns dans PlanningIntervenants.jsx

--- src/pages/PlanningIntervenants.jsx
+++ src/pages/PlanningIntervenants.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { API_URL } from '../config';
 import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
 import { fr } from 'date-fns/locale';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useNavigate, Link } from 'react-router-dom';
